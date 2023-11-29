2bead289807540b9a7410915b699ea4fbb0cb80d
dwiall <148739325+dwiall@users.noreply.github.com>
2023-11-29 20:39:48 +0700

    Add files via upload

---
+++ PBOLat58.java
@@ -0,0 +1,19 @@
+/**
+ *
+ * @author 
+ * Nama     : Dwi Ayu Lestari
+ * Prodi    : Sistem Informasi - Reg. Pagi
+ * NIM      : 22166030
+ */
+public class PBOLat58 {
+    public static void main(String[] args) {
+        int a = 3;
+        int b = 4;
+
+        int penjumlahan = a + b;
+        int selisih = a - b;
+
+        System.out.println("Hasil penjumlahan = " + penjumlahan);
+        System.out.println("Hasil Selisih " + a + " - " + b + " = " + selisih);
+    }
+}
